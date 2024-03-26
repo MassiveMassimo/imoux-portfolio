b6d5861c7b16d02890907fb102e58bf3b3b9eb22
Imo Madjid <mhmmadjid@gmail.com>
2024-03-26 13:17:22 +0700

    fix: trying to fix supabase connection error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main className="flex min-h-svh flex-col items-center gap-10 bg-white p-24 dark:bg-slate-900">
-      <h1 className="text-5xl font-600">Hello World</h1>
+      <h1 className="text-5xl font-600">Press / to chat</h1>
       <Button className="font-400">Click me</Button>
     </main>
   );
