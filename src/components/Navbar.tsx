b4a21f21eccdee3a448e7eaf999938518334ceb3
Imo Madjid <mhmmadjid@gmail.com>
2024-10-17 22:24:03 +0700

    fix: import error in nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 
 import { cn } from "@/lib/utils";
 import Logo from "./Logo";
-import { ThemeToggle } from "./ThemeToggle";
 import { buttonVariants } from "./ui/button";
 
 export default function Navbar() {
