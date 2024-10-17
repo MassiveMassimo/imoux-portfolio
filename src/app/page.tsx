4af1373ddcd656ca0f03c67b231a3ebd567f6f4a
Imo Madjid <mhmmadjid@gmail.com>
2024-10-17 22:22:20 +0700

    fix: import error on footer theme toggler

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
     <main className="flex min-h-svh flex-col">
       <div className="flex h-svh flex-col items-center justify-center p-24">
         <p className="slate-700 dark:slate-300 text-balance text-3xl">
-          I’m Imo. I craft experiences that impact millions through thoughtful
-          design and engineering.
+          I&apos;m Imo. I craft experiences that impact millions through
+          thoughtful design and engineering.
         </p>
       </div>
     </main>
