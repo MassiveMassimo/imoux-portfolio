4c80535f7ffff02cdb25429044e92103bd601554
Imo Madjid <mhmmadjid@gmail.com>
2024-10-17 22:09:28 +0700

    feat: initial hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,13 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-svh flex-col items-center gap-10 p-24">
+    <main className="flex min-h-svh flex-col">
+      <div className="flex h-svh flex-col items-center justify-center p-24">
+        <p className="slate-700 dark:slate-300 text-balance text-3xl">
+          I’m Imo. I craft experiences that impact millions through thoughtful
+          design and engineering.
+        </p>
+      </div>
     </main>
   );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="px-20 pb-24">
       <div className="flex items-center justify-between">
-        <p className="text-slate-700 dark:text-slate-300">
+        <p className="text-slate-400 dark:text-slate-500">
           Designed and developed by Imo
         </p>
         <div className="h-10">
